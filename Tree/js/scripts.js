1cc0ba96dc1e85bb0511584f8d2921e6555a118a
Matere29 <125877628+Matere29@users.noreply.github.com>
2023-04-06 15:36:51 +0200

    Add files via upload

---
+++ Tree/js/scripts.js
@@ -0,0 +1,10 @@
+
+let number = 1;
+let x = '*';
+//declared two variables, number which acts as a counter, and X which holds a symbol we want to display
+for(number = 1; number<=4; number++){
+
+  console.log(x);
+  x = x + '*';
+  
+}
