c349915ea77194832cc66275bbe979bea9d3753b
Matere29 <125877628+Matere29@users.noreply.github.com>
2023-04-13 16:12:20 +0200

    Add files via upload

---
+++ RORMAK879_BCL2302_OWEN_GROUP_Rorisang_Makgaka_ITW11/IWS_11/Js/Scripts.js
@@ -0,0 +1,72 @@
+// script.js
+
+/*
+//const root1 = document.querySelector('order1')
+const biscuits1 = document.getElementById('data-biscuits')
+const donuts1= document.getElementById('data-donuts')
+const pancakes1= document.getElementById('data-pancakes')
+const status= document.getElementById('status')
+
+
+const root2 = document.querySelector('order2')
+const biscuits2= document.querySelector('data-biscuits')
+const donuts2= document.getElementById('data-donuts')
+const pancakes2= document.getElementById('data-pancakes')
+const status2 = document.getElementById('status')
+
+const root3 = document.querySelector('order3')
+const biscuits3= document.getElementById('data-biscuits')
+const donuts3= document.getElementById('data-donuts')
+const pancakes3= document.getElementById('data-pancakes')
+const status3= document.getElementById('status')
+*/
+const root1 = { 
+biscuits: document.getElementById('data-biscuits'),
+donuts: document.getElementById('data-donuts'),
+pancakes: document.getElementById('data-pancakes'),
+status: document.getElementById('status')
+}
+const root2 = { 
+biscuits: document.getElementById('data-biscuits'),
+donuts: document.getElementById('data-donuts'),
+pancakes: document.getElementById('data-pancakes'),
+status: document.getElementById('status')
+}
+
+const root3 = { 
+biscuits: document.getElementById('data-biscuits'),
+donuts: document.getElementById('data-donuts'),
+pancakes: document.getElementById('data-pancakes'),
+status: document.getElementById('status')
+}
+
+biscuits1= root1.biscuits;
+donuts1 = root1.donuts;
+pancakes1 = root1.pancakes;
+console.log(biscuits1)
+
+
+biscuits2= root2.biscuits
+donuts2 = root2.donuts
+pancakes2 = root2.pancakes
+//console.log(status2 = root2.status ? Delivered : Pending)
+
+
+biscuits3= root3.biscuits
+donuts3 = root3.donuts
+pancakes3 = root3.pancakes
+//console.log(status3 = root3.status ? Delivered : Pending)
+
+/*
+const order1 = document.querySelector('[data-key="order1"]');
+//console.log(order1.getAttribute('order1'))
+console.log(order1.getAttribute('data-biscuits')  + order1.getAttribute('data-donuts')  + order1.getAttribute('data-pancakes'));
+
+const order2 = document.querySelector('[data-key="order2"]');
+//console.log(order2.getAttribute('data-key'))
+console.log(order2.getAttribute('data-biscuits') + '-' + order2.getAttribute('data-donuts') + '-' + order2.getAttribute('data-pancakes'));
+
+const order3 = document.querySelector('[data-key="order3"]');
+//console.log(order3.getAttribute('data-key'))
+console.log(order3.getAttribute('data-biscuits') + '-' + order3.getAttribute('data-donuts') + '-' + order3.getAttribute('data-pancakes'));
+*/
